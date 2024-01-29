129e3bcb3dd7d01cb86013ac4de80089cef775a9
Lutto <118007877+StillLutto@users.noreply.github.com>
2024-01-29 21:17:40 +0100

    feat: make custom player join message better

--- src/main/java/com/lutto/upblock/listeners/PlayerJoinListener.java
+++ src/main/java/com/lutto/upblock/listeners/PlayerJoinListener.java
@@ -16,6 +16,11 @@ public class PlayerJoinListener implements Listener {
         Player player = event.getPlayer();
         Integer totalPlayersJoined = Bukkit.getOfflinePlayers().length + Bukkit.getOnlinePlayers().size() - 1; // need to -1 because it gets the player that joins as offline and online
 
+        if (player.hasPlayedBefore()) {
+            event.setJoinMessage(ChatColor.DARK_AQUA + "Welcome to the server " + ChatColor.AQUA + player.getDisplayName() + ChatColor.DARK_AQUA + "!");
+            return;
+        }
+
         StringBuilder joinMessage = new StringBuilder();
         joinMessage.append(ChatColor.DARK_AQUA + "Welcome to the server " + ChatColor.BLUE + player.getDisplayName() + ChatColor.DARK_AQUA + "!").append("\n");
         joinMessage.append(ChatColor.AQUA + "Congrats! You are our #" + totalPlayersJoined + " player joined!");
